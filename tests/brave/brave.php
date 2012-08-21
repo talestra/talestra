06061ba0c7533b75447399f8294fd5a1e29c64d2
soywiz <soywiz@gmail.com>
2012-08-21 03:25:36 +0200

    - Done!

--- tests/brave/brave.php
+++ tests/brave/brave.php
@@ -34,16 +34,101 @@ function decrypt_data($data) {
 	return $data;
 }
 
+function extractScale($v, $offset, $count, $to) {
+	$mask = ((1 << $count) - 1);
+	//printf("%d\n", $mask);
+	return ((($v >> $offset) & $mask) * $to) / $mask;
+}
+
+function decodeImage($f) {
+	static $key1 = [0x84, 0x41, 0xDE, 0x48, 0x08, 0xCF, 0xCF, 0x6F, 0x62, 0x51, 0x64, 0xDF, 0x41, 0xDF, 0xE2, 0xE1];
+
+	//$f = fopen('PT_LOGO.CRP.u', 'rb');
+	//$f = fopen('P_CURSOR.CRP.u', 'rb');
+	$magic = fread($f, 14);
+	if ($magic != "(C)CROWD ARPG\0") throw(new Exception("Invalid file '{$magic}'"));
+	$key    = fread($f, 0x08);
+	$header = fread($f, 0x10);
+
+	//printf("----------------\n");
+
+	// STEP1
+	for ($n = 0; $n < 0x10; $n++) {
+		$v2 = decrypt_data_rr($key1[$n], ord($header[$n]));
+		//printf("%02X -> %02X\n", ord($header[$n]), $v2);
+		$header[$n] = chr($v2);
+	}
+
+	//printf("----------------\n");
+
+	// STEP2
+	for ($n = 0; $n < 0x10; $n++) {
+		$v2 = decrypt_data_rr(ord($header[$n]), ord($key[$n % 8]));
+		//printf("%02X -> %02X\n", ord($header[$n]), $v2);
+		$header[$n] = chr($v2);
+	}
+
+	//printf("----------------\n");
+
+	list(,$width, $height, $skip) = unpack('V3', $header);
+
+	//printf("%d, %d, %d\n", $width, $height, $skip);
+
+	fseek($f, $skip, SEEK_CUR);
+
+	// INT32 : Width
+	// INT32 : Height
+	// INT32 : Skip
+
+	$i = imagecreatetruecolor($width, $height);
+	$data = fread($f, $width * $height * 2);
+	$n = 0;
+	for ($y = 0; $y < $height; $y++) {
+		for ($x = 0; $x < $width; $x++) {
+			list(,$pixelData) = unpack('v', substr($data, $n * 2, 2));
+			$b = extractScale($pixelData, 0, 5, 255);
+			$g = extractScale($pixelData, 5, 6, 255);
+			$r = extractScale($pixelData, 11, 5, 255);
+			//printf("%04X, %d, %d, %d\n", $pixelData, $r, $g, $b);
+			imagesetpixel($i, $x, $y, imagecolorallocate($i, $r, $g, $b));
+			$n++;
+		}
+	}
+
+	//imagepng($i, 'test.png');
+	return $i;
+}
+
 $gameFolder = 'c:/juegos/brave_s';
 
 foreach (glob("{$gameFolder}/scenario/*.dat") as $fileIn) {
-	if (substr($fileIn, -4) != '.dat') continue;
-
 	$fileOut = substr($fileIn, 0, -4) . '.scr';
 	
 	echo "{$fileIn} -> {$fileOut}\n";
 
-	file_put_contents($fileOut, decrypt_data(file_get_contents($fileIn)));
-
+	if (!file_exists($fileOut)) {
+		file_put_contents($fileOut, decrypt_data(file_get_contents($fileIn)));
+	}
 }
 
+foreach (glob("{$gameFolder}/map/*.dat") as $fileIn) {
+	$fileOut = substr($fileIn, 0, -4) . '.scr';
+	
+	echo "{$fileIn} -> {$fileOut}\n";
+
+	if (!file_exists($fileOut)) {
+		file_put_contents($fileOut, decrypt_data(file_get_contents($fileIn)));
+	}
+}
+
+foreach (glob("{$gameFolder}/parts/*.CRP") as $fileIn) {
+	$fileOut1 = substr($fileIn, 0, -4) . '.CRP.u';
+	$fileOut2 = substr($fileIn, 0, -4) . '.PNG';
+	
+	echo "{$fileIn} -> {$fileOut2}\n";
+	
+	if (!file_exists($fileOut2)) {
+		`ms-expand.exe {$fileIn} {$fileOut1}`;
+		imagepng(decodeImage(fopen($fileOut1, 'rb')), $fileOut2);
+	}
+}
--- tests/brave/decode_image.php
+++ tests/brave/decode_image.php
@@ -1,6 +1,9 @@
 <?php
 
-$i = imagecreatetruecolor(640, 480);
+
+function decrypt_data_rr($cl, $dl) {
+	return ~($cl & $dl) & ($cl | $dl);
+}
 
 function extractScale($v, $offset, $count, $to) {
 	$mask = ((1 << $count) - 1);
@@ -8,18 +11,63 @@ function extractScale($v, $offset, $count, $to) {
 	return ((($v >> $offset) & $mask) * $to) / $mask;
 }
 
-$data = file_get_contents('test_color.bin');
-$n = 0;
-for ($y = 0; $y < 480; $y++) {
-	for ($x = 0; $x < 640; $x++) {
-		list(,$pixelData) = unpack('v', substr($data, $n * 2, 2));
-		$b = extractScale($pixelData, 0, 5, 255);
-		$g = extractScale($pixelData, 5, 6, 255);
-		$r = extractScale($pixelData, 11, 5, 255);
-		//printf("%04X, %d, %d, %d\n", $pixelData, $r, $g, $b);
-		imagesetpixel($i, $x, $y, imagecolorallocate($i, $r, $g, $b));
-		$n++;
+function decodeImage($f) {
+	static $key1 = [0x84, 0x41, 0xDE, 0x48, 0x08, 0xCF, 0xCF, 0x6F, 0x62, 0x51, 0x64, 0xDF, 0x41, 0xDF, 0xE2, 0xE1];
+
+	//$f = fopen('PT_LOGO.CRP.u', 'rb');
+	//$f = fopen('P_CURSOR.CRP.u', 'rb');
+	if (fread($f, 14) != "(C)CROWD ARPG\0") throw(new Exception("Invalid file"));
+	$key    = fread($f, 0x08);
+	$header = fread($f, 0x10);
+
+	//printf("----------------\n");
+
+	// STEP1
+	for ($n = 0; $n < 0x10; $n++) {
+		$v2 = decrypt_data_rr($key1[$n], ord($header[$n]));
+		//printf("%02X -> %02X\n", ord($header[$n]), $v2);
+		$header[$n] = chr($v2);
 	}
+
+	//printf("----------------\n");
+
+	// STEP2
+	for ($n = 0; $n < 0x10; $n++) {
+		$v2 = decrypt_data_rr(ord($header[$n]), ord($key[$n % 8]));
+		//printf("%02X -> %02X\n", ord($header[$n]), $v2);
+		$header[$n] = chr($v2);
+	}
+
+	//printf("----------------\n");
+
+	list(,$width, $height, $skip) = unpack('V3', $header);
+
+	//printf("%d, %d, %d\n", $width, $height, $skip);
+
+	fseek($f, $skip, SEEK_CUR);
+
+	// INT32 : Width
+	// INT32 : Height
+	// INT32 : Skip
+
+	$i = imagecreatetruecolor($width, $height);
+	$data = fread($f, $width * $height * 2);
+	$n = 0;
+	for ($y = 0; $y < $height; $y++) {
+		for ($x = 0; $x < $width; $x++) {
+			list(,$pixelData) = unpack('v', substr($data, $n * 2, 2));
+			$b = extractScale($pixelData, 0, 5, 255);
+			$g = extractScale($pixelData, 5, 6, 255);
+			$r = extractScale($pixelData, 11, 5, 255);
+			//printf("%04X, %d, %d, %d\n", $pixelData, $r, $g, $b);
+			imagesetpixel($i, $x, $y, imagecolorallocate($i, $r, $g, $b));
+			$n++;
+		}
+	}
+
+	//imagepng($i, 'test.png');
+	return $i;
 }
 
+$i = decodeImage(fopen('PT_LOGO.CRP.u', 'rb'));
 imagepng($i, 'test.png');
